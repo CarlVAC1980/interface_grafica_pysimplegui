85f38bca3c8b53eed49ffedc3e203150782666c7
Carlos M Valenca <cavac10@gmail.com>
2022-08-22 08:30:08 -0300

    Estou no output 19:27

--- tela.py
+++ tela.py
@@ -9,31 +9,33 @@ class TelaPython:
             [sg.Text('Idade', size=(5, 0)), sg.Input(size=(15, 0), key='idade')],
             [sg.Text('Quais provedores de e-mail são aceitos?')],
             [sg.Checkbox('Gmail', key='gmail'), sg.Checkbox('Outlook', key='outlook'),
-            sg.Checkbox('Yahoo', key='yahoo')],
+             sg.Checkbox('Yahoo', key='yahoo')],
             [sg.Text('Aceita cartão')],
-            [sg.Radio('Sim', 'cartoes', key='aceitaCartao'),sg.Radio('Não', 'cartoes', key='naoAceitaCartao')],
-            [sg.Button('Enviar Dados')]
+            [sg.Radio('Sim', 'cartoes', key='aceitaCartao'), sg.Radio('Não', 'cartoes', key='naoAceitaCartao')],
+            [sg.Button('Enviar Dados')],
+            [sg.Output(size=(30,20))]
         ]
         """Janela"""
-        janela = sg.Window("Dados do Usuário").layout(layout)
-        """Extrair os dados da tela"""
-        self.button, self.values = janela.Read()
+        self.janela = sg.Window("Dados do Usuário").layout(layout)
 
     def Iniciar(self):
-        nome = self.values['nome']
-        idade = self.values['idade']
-        aceita_gmail = self.values['gmail']
-        aceita_outlook = self.values['outlook']
-        aceita_yahoo = self.values['yahoo']
-        aceita_cartao = self.values['aceitaCartao']
-        nao_aceita_cartao = self.values['naoAceitaCartao']
-        print(f'nome: {nome}')
-        print(f'idade: {idade}')
-        print(f'aceita gmail: {aceita_gmail}')
-        print(f'aceita outlook: {aceita_outlook}')
-        print(f'aceita yahoo: {aceita_yahoo}')
-        print(f'aceita cartao: {aceita_cartao}')
-        print(f'nao aceita cartao: {nao_aceita_cartao}')
+        while True:
+            """Extrair os dados da tela"""
+            self.button, self.values = self.janela.Read()
+            nome = self.values['nome']
+            idade = self.values['idade']
+            aceita_gmail = self.values['gmail']
+            aceita_outlook = self.values['outlook']
+            aceita_yahoo = self.values['yahoo']
+            aceita_cartao = self.values['aceitaCartao']
+            nao_aceita_cartao = self.values['naoAceitaCartao']
+            print(f'nome: {nome}')
+            print(f'idade: {idade}')
+            print(f'aceita gmail: {aceita_gmail}')
+            print(f'aceita outlook: {aceita_outlook}')
+            print(f'aceita yahoo: {aceita_yahoo}')
+            print(f'aceita cartao: {aceita_cartao}')
+            print(f'nao aceita cartao: {nao_aceita_cartao}')
 
 
 tela = TelaPython()
